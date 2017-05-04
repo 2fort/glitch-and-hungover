7bf16aebf61763420d48ff11ee8a543db1701dca
Alexander Kirsanov <nullcallback@gmail.com>
2017-05-04 19:24:11 +0300

    index quick fix

--- src/components/ImageViewer/index.jsx
+++ src/components/ImageViewer/index.jsx
@@ -295,7 +295,7 @@ class ImageViewer extends Component {
             className={css.fullimg}
             alt={currentImg}
             key={`${currentImg} full`}
-            src={`/img/comics/${images[currentImg - 1].large}`}
+            src={`/img/comics/${images[currentImg - 1].medium}`}
             ref={(el) => { this.dom.image = el; }}
             onLoad={this.hidePreview}
             onWheel={mouse.handleWheel(initial, this.current, this.apply)}
